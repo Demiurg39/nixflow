d02a27adfce51d84c365fdb851889c3ec6c10ee3
Demiurg39 <chiryagov2014@gmail.com>
2024-12-30 00:46:53 +0300

    refactored packages

--- nixos/modules/games.nix
+++ nixos/modules/games.nix
@@ -6,26 +6,24 @@
     # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play 
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    gamescopeSession.enable = false;
+    gamescopeSession.enable = true;
   };
   
   # Lutris
   environment.systemPackages = with pkgs; [
     wineWowPackages.stable
+    wine
     protonup-qt
     steamtinkerlaunch
+    gamescope
+    mangohud
 
     (lutris.override {
       extraLibraries = pkgs: [
       	protobuf
-      	dxvk
-	vkd3d
 	libvdpau
       ];
-      extraPkgs = pkgs: [
-	gamescope
-	mangohud
-      ];
+      extraPkgs = pkgs: [];
     })
   ];
 

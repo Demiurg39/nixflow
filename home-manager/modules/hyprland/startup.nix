414dd5492191cf4cdbac4daf4983e92bbb7de23a
Demiurg39 <chiryagov2014@gmail.com>
2024-12-31 21:41:23 +0300

    startup added for hypr
    colors renamed to theme

--- home-manager/modules/hyprland/startup.nix
+++ home-manager/modules/hyprland/startup.nix
@@ -1,8 +1,6 @@
 {
   wayland.windowManager.hyprland.settings = {
 
-      "$srcPath" = "~/.local/bin";
-
       exec-once = [
 
       # █░░ ▄▀█ █░█ █▄░█ █▀▀ █░█
@@ -12,49 +10,31 @@
 
       # "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP" # for XDPH
      
-      # exec-once = $scrPath/resetxdgportal.sh # reset XDPH for screenshare
-      # exec-once = /usr/lib64/libexec/polkit-kde-authentication-agent-1 # kde authentication agent
-      # exec-once = $scrPath/polkitkdeauth.sh # authentication dialogue for GUI apps
-      # exec-once = waybar # launch the system bar
-      # exec-once = hypridle # handles afk(low brightness, lock session, turn off screen)
-      # exec-once = blueman-applet # systray app for Bluetooth
-      # exec-once = udiskie --no-automount --smart-tray # front-end that allows to manage removable media
-      # exec-once = nm-applet --indicator # systray app for Network/Wifi
-      # exec-once = dunst # start notification demon
-      # exec-once = wlsunset -t 5200 -S 8:00 -s 20:30 # NightLight from 8.30pm to 8.00am
-      # exec-once = wl-paste --type text --watch cliphist store # clipboard store text data
-      # exec-once = wl-paste --type image --watch cliphist store # clipboard store image data
-      # exec-once = swww-daemon
-      # exec-once = $scrPath/swwwallpaper.sh # start wallpaper daemon
-      # exec-once = $scrPath/batterynotify.sh # battery notification
-      # exec-once = $scrPath/idle.sh # lockscreen after 5 minutes, after 10 sleep
+      # waybar # launch the system bar
+      # hypridle # handles afk(low brightness, lock session, turn off screen)
+      # blueman-applet # systray app for Bluetooth
+      # udiskie --no-automount --smart-tray # front-end that allows to manage removable media
+      # nm-applet --indicator # systray app for Network/Wifi
+      # dunst # start notification demon
+      # wlsunset -t 5200 -S 8:00 -s 20:30 # NightLight from 8.30pm to 8.00am
+
+      "wl-paste --type text --watch cliphist store"; # clipboard store text data
+      "wl-paste --type image --watch cliphist store"; # clipboard store image data
+      "swww-daemon --format xrgb"; # wallpapers daemon
+      # "hypridle"; # idling
 
       # Bar, wallpaper
-      # exec-once = swww-daemon --format xrgb
-      # exec-once = /usr/lib/geoclue-2.0/demos/agent & gammastep
-      # exec-once = ags &
-      #
-      # # Input method
-      # exec-once = fcitx5
-      #
+      # /usr/lib/geoclue-2.0/demos/agent & gammastep
+      # ags &
+      
       # # Core components (authentication, lock screen, notification daemon)
-      # exec-once = gnome-keyring-daemon --start --components=secrets
-      # exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 || /usr/libexec/polkit-gnome-authentication-agent-1
-      # exec-once = hypridle
-      # exec-once = dbus-update-activation-environment --all
-      # exec-once = sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # Some fix idk
-      # exec-once = hyprpm reload
-      #
+      # gnome-keyring-daemon --start --components=secrets
+      # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 || /usr/libexec/polkit-gnome-authentication-agent-1
+      # dbus-update-activation-environment --all
+      # hyprpm reload
+
       # # Audio
-      # exec-once = easyeffects --gapplication-service
-      #
-      # # Clipboard: history
-      # # exec-once = wl-paste --watch cliphist store &
-      # exec-once = wl-paste --type text --watch cliphist store
-      # exec-once = wl-paste --type image --watch cliphist store
-      #
-      # # Cursor
-      # exec-once = hyprctl setcursor Bibata-Modern-Classic 24
+      # easyeffects --gapplication-service
 
       ];
   };

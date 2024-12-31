d4330177cf08be8a633d4f937badb8902697a4f7
Demiurg39 <chiryagov2014@gmail.com>
2024-12-31 12:56:50 +0300

    move env vars to uwsm conf file
    hyprland basics was removed due to using uwsm as it auto export needed
    vars

--- home-manager/modules/hyprland/env.nix
+++ home-manager/modules/hyprland/env.nix
@@ -1,45 +1,38 @@
 {
-  wayland.windowManager.hyprland.settings = {
-    env = [
-      # --- Hyprland basics vars ---
-      "XDG_CURRENT_DESKTOP,Hyprland"
-      "XDG_SESSION_TYPE,wayland"
-      "XDG_SESSION_DESKTOP,Hyprland"
 
+  home.file.".config/uwsm/env".text = ''
       # --- Toolkit backend vars ---
-      "GDK_BACKEND,wayland,x11,*"
-      "QT_QPA_PLATFORM,wayland;xcb"
-      # "SDL_VIDEODRIVER,wayland" # cause nvidia driver problems
-      "CLUTTER_BACKEND,wayland"
-      "_JAVA_AWT_WM_NONREPARENTING,1"
+      export GDK_BACKEND=wayland,x11,*
+      export QT_QPA_PLATFORM=wayland;xcb
+      # export SDL_VIDEODRIVER=wayland # cause nvidia driver problems
+      # "_JAVA_AWT_WM_NONREPARENTING,1"
 
       # --- Qt vars ---
-      "QT_AUTO_SCREEN_SCALE_FACTOR,1"
-      "OZONE_PLATFORM,wayland"
-      "QT_QPA_PLATFORMTHEME,qt5ct"
-      "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+      export QT_AUTO_SCREEN_SCALE_FACTOR=1
+      export QT_QPA_PLATFORMTHEME=qt5ct
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      # "OZONE_PLATFORM,wayland"
 
-      # idk what is this
-      "MOZ_ENABLE_WAYLAND,1"
-      "MOZ_DISABLE_RDD_SANDBOX,1"
-      "GDK_SCALE,1"
-     
+      export MOZ_ENABLE_WAYLAND=1 # enable for librewolf and firefox
+      # "MOZ_DISABLE_RDD_SANDBOX,1"
+      # "GDK_SCALE,1"
+
       # --- Cursor ---
-      "HYPRCURSOR_THEME,Bibata-Modern-Classic"
-      "HYPRCURSOR_SIZE,24"
-      "XCURSOR_THEME,\"Bibata-Modern-Classic\""
-      "XCURSOR_SIZE,24 "
+      export HYPRCURSOR_THEME=Bibata-Modern-Classic
+      export HYPRCURSOR_SIZE=24
+      export XCURSOR_THEME=Bibata-Modern-Classic
+      export XCURSOR_SIZE=24 
 
       # █▄░█ █░█ █ █▀▄ █ ▄▀█
       # █░▀█ ▀▄▀ █ █▄▀ █ █▀█
 
       # See https://wiki.hyprland.org/Nvidia/
-      "LIBVA_DRIVER_NAME,nvidia"
-      "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-      "__GL_VRR_ALLOWED,1"
-      "GBM_BACKEND,nvidia-drm"
+      export LIBVA_DRIVER_NAME=nvidia
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export GBM_BACKEND=nvidia-drm"
+      # export __GL_VRR_ALLOWED=1"
       # "WLR_DRM_NO_ATOMIC,1"
       # "WLR_NO_HARDWARE_CURSORS,1"
-    ];
-  };
+  '';
+
 }

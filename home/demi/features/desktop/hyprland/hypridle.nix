bdc4c0fcfa77c7330407714977c9d85bbd03f0f2
Demiurg39 <chiryagov2014@gmail.com>
2025-02-10 12:48:01 +0300

    hyprland: hyprlock and hypridle added

---
+++ home/demi/features/desktop/hyprland/hypridle.nix
@@ -0,0 +1,37 @@
+{...}: {
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hypridle-background";
+        before_sleep_cmd = "loginctl lock-session";
+      };
+
+      listener = [
+        {
+          timeout = 180;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+        {
+          timeout = 180;
+          on-timeout = "brightnessctl -sd asus::kbd_backlight set 0";
+          on-resume = "brightnessctl -rd asus::kbd_backlight";
+        }
+        {
+          timeout = 300;
+          on-timeout = "pidof hyprlock || hypridle-background";
+        }
+        {
+          timeout = 480;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 540;
+          on-timeout = "pidof steam || systemctl suspend || loginctl suspend";
+        }
+      ];
+    };
+  };
+}

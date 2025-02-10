100e493b89184d461a23e2406a785ecbeb62c50f
Demiurg39 <chiryagov2014@gmail.com>
2025-02-10 12:46:32 +0300

    theme: oneui icons added

--- home/demi/features/desktop/common/theme.nix
+++ home/demi/features/desktop/common/theme.nix
@@ -1,7 +1,25 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  ags = inputs.ags.packages.${pkgs.system}.default.override {
+    extraPackages = with pkgs; [
+      gtksourceview
+      gtksourceview4
+      webkitgtk
+      webp-pixbuf-loader
+      ydotool
+    ];
+  };
+  selfPkgs = import ../../../../../pkgs/illogical-impulse.nix {
+    inherit pkgs ags;
+  };
+in {
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
+    selfPkgs.illogical-impulse-oneui4-icons
     adwaita-qt6
     adw-gtk3
     bibata-cursors
@@ -21,4 +39,22 @@
   };
 
   home.file.".local/share/icons/MoreWaita".source = "${pkgs.morewaita-icon-theme}/share/icons";
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+  };
+  qt = {
+    enable = true;
+    platformTheme.name = "qt5ct";
+    style.name = "kvantum";
+  };
+
+  home.file.".config/Kvantum" = {
+    source = "${selfPkgs.illogical-impulse-kvantum}";
+    recursive = true;
+  };
 }

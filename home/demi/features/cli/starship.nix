fed95f62f8d9cdfb8b87fd6b9e3c3d8ad5227fcd
Demiurg39 <chiryagov2014@gmail.com>
2025-03-27 10:36:55 +0300

    cli: starship prompt moved inside zsh module

--- home/demi/features/cli/starship.nix
+++
@@ -1,8 +0,0 @@
-{config, ...}: {
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = config.features.cli.zsh.enable == true;
-    enableNushellIntegration = config.features.cli.nushell.enable == true;
-    # settings = {};
-  };
-}

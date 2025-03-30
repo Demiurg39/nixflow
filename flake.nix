13b08bc7a635f5dae1475f5d811b50303c5babd9
Demiurg39 <chiryagov2014@gmail.com>
2025-03-30 17:12:33 +0300

    new: pre-commit-hooks

--- flake.nix
+++ flake.nix
@@ -1,29 +1,6 @@
 {
   description = "My system configuration flake";
 
-  inputs = {
-    systems.url = "github:nix-systems/default-linux";
-
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
-
-    nivix.url = "github:demiurg39/nivix";
-    nivix.inputs.nixpkgs.follows = "nixpkgs";
-
-    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    ags.url = "github:Aylur/ags/v1";
-    ags.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
   outputs = {flake-parts, ...} @ inputs:
     flake-parts.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
@@ -31,7 +8,7 @@
       imports = [
         # TODO:
         # ./hosts
-        # ./pre-commit-hooks.nix
+        ./pre-commit-hooks.nix
       ];
 
       perSystem = {
@@ -53,26 +30,31 @@
 
         formatter = pkgs.alejandra;
       };
-
-      # nixosConfigurations = {
-      #   # asus laptop
-      #   asura = lib.nixosSystem {
-      #     specialArgs = {
-      #       inherit inputs;
-      #       inherit outputs;
-      #       inherit username;
-      #       inherit hostname;
-      #     };
-      #     modules = [./hosts/asura];
-      #   };
-      # };
-
-      # homeConfigurations = {
-      #   demi = lib.homeManagerConfiguration {
-      #     pkgs = pkgsFor.x86_64-linux;
-      #     extraSpecialArgs = {inherit inputs outputs;};
-      #     modules = [./home/demi/asura.nix];
-      #   };
-      # };
     };
+
+  inputs = {
+    systems.url = "github:nix-systems/default-linux";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.inputs.nixpkgs.follows = "nixpkgs";
+
+    nivix.url = "github:demiurg39/nivix";
+    nivix.inputs.nixpkgs.follows = "nixpkgs";
+
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    ags.url = "github:Aylur/ags/v1";
+    ags.inputs.nixpkgs.follows = "nixpkgs";
+  };
 }

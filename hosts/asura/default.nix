d900d3446534339a4e9a51c3aefb2100ac04425f
Demiurg39 <chiryagov2014@gmail.com>
2025-03-31 11:56:40 +0300

    refactor: removed imports
    added also FLAKE shell variable and fstrim service as this host have
    nvme

--- hosts/asura/default.nix
+++ hosts/asura/default.nix
@@ -1,30 +1,16 @@
 {pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
-
-    ../common/global
-    ../common/users/demi
-
-    ../common/optional/android.nix
-    ../common/optional/gamemode.nix
-    ../common/optional/hyprland.nix
-    ../common/optional/kanata.nix
-    ../common/optional/nvidia-gpu.nix
-    ../common/optional/pipewire.nix
-    ../common/optional/polkit.nix
-    ../common/optional/quietboot.nix
-    ../common/optional/sddm.nix
-    ../common/optional/ssd.nix
-    ../common/optional/steam.nix
-    ../common/optional/warp.nix
-    ../common/optional/wireless.nix
   ];
+
+  # nh default flake
+  environment.variables.FLAKE = "/home/demi/nixflow";
+
+  # for SSD/NVME health
+  services.fstrim.enable = true;
 
   # hostname
   networking.hostName = "asura";
-
-  # git
-  environment.systemPackages = with pkgs; [git];
 
   system.stateVersion = "24.11"; # Do not touch
 }

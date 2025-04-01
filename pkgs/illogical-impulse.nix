96bafd2ba4dc375072e7c1d9c0a690679ee42e97
Demiurg39 <chiryagov2014@gmail.com>
2025-04-01 10:45:44 +0300

    refactor: sub-pkgs moved inside illogical-impulse

--- pkgs/illogical-impulse.nix
+++ pkgs/illogical-impulse.nix
@@ -6,8 +6,8 @@
   inherit (pkgs) lib;
 in
   lib.fix (self: {
-    illogical-impulse-ags = pkgs.callPackage ./illogical-impulse-ags {};
-    illogical-impulse-ags-launcher = pkgs.callPackage ./illogical-impulse-ags-launcher {inherit ags;};
-    illogical-impulse-kvantum = pkgs.callPackage ./illogical-impulse-kvantum {};
-    illogical-impulse-oneui4-icons = pkgs.callPackage ./illogical-impulse-oneui4-icons {};
+    illogical-impulse-ags = pkgs.callPackage ./illogical-impulse {};
+    illogical-impulse-ags-launcher = pkgs.callPackage ./illogical-impulse/ags-launcher {inherit ags;};
+    illogical-impulse-kvantum = pkgs.callPackage ./illogical-impulse/kvantum {};
+    illogical-impulse-oneui4-icons = pkgs.callPackage ./illogical-impulse/oneui4-icons {};
   })

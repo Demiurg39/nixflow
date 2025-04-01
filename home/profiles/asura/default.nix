faa60119dee9a52cae54f10f907ef9248788bb67
Demiurg39 <chiryagov2014@gmail.com>
2025-04-01 11:32:49 +0300

    refactor: moved to profiles

---
+++ home/profiles/asura/default.nix
@@ -0,0 +1,32 @@
+{self, ...}: let
+  home = "${self}/home";
+in {
+  imports = [
+    "${home}/editors/nivix.nix"
+
+    "${home}/programs"
+    "${home}/programs/games"
+
+    "${home}/programs/wayland"
+    "${home}/programs/wayland/hyprland"
+    #
+    "${home}/terminal/programs"
+    "${home}/terminal/emulators/kitty.nix"
+
+    "${home}/terminal/shell/nushell"
+    "${home}/terminal/shell/atuin.nix"
+    "${home}/terminal/shell/zoxide.nix"
+    #
+    "${home}/services/media/playerctl.nix"
+
+    "${home}/services/system/polkit-agent.nix"
+    "${home}/services/system/udiskie.nix"
+
+    "${home}/services/wayland/hypridle.nix"
+    "${home}/services/wayland/wlsunset.nix"
+  ];
+
+  wayland.windowManager.hyprland.settings.monitor = [
+    "eDP-1,1920x1080@144,0x0,1"
+  ];
+}

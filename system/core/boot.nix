6d4f81bb70bd813b5dd513123f055117a02ce480
Demiurg39 <chiryagov2014@gmail.com>
2025-04-01 16:45:44 +0300

    tweak: changed grub to systemd for future using of lanzaboote

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -5,16 +5,17 @@
     # use latest kernel
     kernelPackages = pkgs.linuxPackages_latest;
 
-    # kernelParams = [
-    #   "quiet"
-    #   "systemd.show_status=auto"
-    #   "rd.udev.log_level=3"
-    # ];
+    kernelParams = [
+      "quiet"
+      "systemd.show_status=auto"
+      "rd.udev.log_level=3"
+    ];
 
-    loader.grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
+    loader = {
+      # systemd-boot on UEFI
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+      systemd-boot.configurationLimit = 5;
     };
 
     plymouth = {

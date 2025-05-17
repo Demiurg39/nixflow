44bb57098ad97f9e1e91f2475118865e31faed67
Demiurg39 <chiryagov2014@gmail.com>
2025-05-17 20:45:12 +0600

    refactor: added mediainfo & exiftool for yazi

--- home/terminal/programs/yazi.nix
+++ home/terminal/programs/yazi.nix
@@ -1,8 +1,17 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.yazi = {
     enable = true;
     enableZshIntegration = config.programs.zsh.enable == true;
     enableNushellIntegration = config.programs.nushell.enable == true;
     # TODO: make further config setup
   };
+
+  home.packages = with pkgs; [
+    mediainfo
+    exiftool
+  ];
 }

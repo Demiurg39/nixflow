e47636bb184ff8b6c6bba55f2e6c35825ac2007d
Demiurg39 <chiryagov2014@gmail.com>
2025-08-27 16:03:04 +0600

    added my nextdns conf to resolved

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,6 +1,12 @@
 {...}: {
   networking = {
-    nameservers = ["9.9.9.9#dns.quad9.net"];
+    nameservers = [
+      "45.90.28.0#a6d793.dns.nextdns.io"
+      "2a07:a8c0::#a6d793.dns.nextdns.io"
+      "45.90.30.0#a6d793.dns.nextdns.io"
+      "2a07:a8c1::#a6d793.dns.nextdns.io"
+      "9.9.9.9#dns.quad9.net"
+    ];
 
     networkmanager = {
       enable = true;
@@ -15,12 +21,6 @@
 
     resolved = {
       enable = true;
-      domains = [
-        "45.90.28.0#a6d793.dns.nextdns.io"
-        "2a07:a8c0::#a6d793.dns.nextdns.io"
-        "45.90.30.0#a6d793.dns.nextdns.io"
-        "2a07:a8c1::#a6d793.dns.nextdns.io"
-      ];
       dnsovertls = "true";
     };
   };

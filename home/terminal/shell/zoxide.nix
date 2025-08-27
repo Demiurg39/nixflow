9f18c0169cf5d630ef2eb263b22783b205a39f68
Demiurg39 <chiryagov2014@gmail.com>
2025-08-27 13:10:47 +0600

    removed '--cmd' option to use defaults z&zi commands

--- home/terminal/shell/zoxide.nix
+++ home/terminal/shell/zoxide.nix
@@ -3,6 +3,7 @@
     enable = true;
     enableZshIntegration = config.programs.zsh.enable == true;
     enableNushellIntegration = config.programs.nushell.enable == true;
-    options = ["--cmd zi"];
+    # NOTE: uses default z&zi cmd
+    options = [];
   };
 }

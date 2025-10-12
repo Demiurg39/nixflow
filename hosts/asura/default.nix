f7d6ea4fb2ad3f90383d918244bf530f0342c894
demiurg39 <chiryagov2014@gmail.com>
2025-10-12 12:55:12 +0600

    reduce size of hosts/<name> files for simplecity and future changes

--- hosts/asura/default.nix
+++ hosts/asura/default.nix
@@ -1,18 +1,17 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./hardware-configuration.nix
+    ./secrets.nix
   ];
 
+  # TODO: move this somewhere more suited for this
+  # cause flake might be used not by just this host
+  # maybe exporting it somewhere else
   # nh default flake
-  environment.variables.FLAKE = "/home/demi/nixflow";
+  # environment.variables.FLAKE = "/home/demi/nixflow";
 
   console.keyMap = "mod-dh-ansi-us";
 
   # for SSD/NVME health
   services.fstrim.enable = true;
-
-  # hostname
-  networking.hostName = "asura";
-
-  system.stateVersion = "24.11"; # Do not touch
 }

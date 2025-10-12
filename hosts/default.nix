453f0b79a845d4818f979c2daaffac2e8ba86af1
demiurg39 <chiryagov2014@gmail.com>
2025-10-12 12:52:15 +0600

    rewrite hosts/default for simplicity and not using flake-parts
    delete old file structure and substituted with new syntax
    old structure temporarely commented and stay still before configuration
    will be fully rewrited as i want

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,61 +1,94 @@
 {
+  nixpkgs,
   self,
-  inputs,
   ...
-}: {
-  flake.nixosConfigurations = let
-    inherit (inputs.nixpkgs.lib) nixosSystem;
-
-    homeImports = import "${self}/home/profiles";
-
-    mod = "${self}/system";
-
-    # get the basic config to build on top of
-    inherit (import mod) laptop;
-
-    # get these into the module system
-    specialArgs = {inherit inputs self;};
-  in {
-    asura = nixosSystem {
-      inherit specialArgs;
+}: let
+  inherit (self) inputs;
+  mkHost = name: system:
+    nixpkgs.lib.nixosSystem {
       modules =
-        laptop
-        ++ [
-          ./asura
-          ./asura/secrets.nix
-
-          "${mod}/core/lanzaboote.nix"
-
-          "${mod}/hardware/nvidia-laptop.nix"
-
-          "${mod}/network/localsend.nix"
-          "${mod}/network/packettracer.nix"
-
-          "${mod}/programs/gamemode.nix"
-          "${mod}/programs/gaming.nix"
-          "${mod}/programs/hyprland"
-          "${mod}/programs/adb.nix"
-          "${mod}/programs/diagnostics.nix"
-          "${mod}/programs/arion.nix"
-
-          "${mod}/services/kanata"
-          "${mod}/services/syncthing.nix"
-          "${mod}/services/postgresql.nix"
-          inputs.agenix.nixosModules.default
-
+        [
           {
+            networking.hostName = nixpkgs.lib.mkDefault name;
+            nixpkgs.hostPlatform = nixpkgs.lib.mkDefault system;
+            nixpkgs.config.allowUnfree = true;
+            nix.settings.experimental-features = ["nix-command" "flakes"];
             environment.systemPackages = [
-              inputs.agenix.packages.x86_64-linux.default
+              inputs.agenix.packages.${system}.default
             ];
           }
+          ./${name}
 
-          {
-            home-manager = {
-              users.demi.imports = homeImports."demi@asura";
-              extraSpecialArgs = specialArgs;
-            };
-          }
-        ];
+          inputs.agenix.nixosModules.default
+        ]
+        ++ builtins.attrValues self.nixosModules;
+
+      # This allows to easily access flake inputs and outputs
+      # from nixos modules, so it's a little bit cleaner
+      specialArgs = {
+        inherit inputs;
+        # TODO: make my theme module
+        # theme = (import ../user).theme nixpkgs.legacyPackages.${system};
+        flake = self;
+      };
     };
-  };
+in {
+  asura = mkHost "asura" "x86_64-linux";
+  #   homeImports = import "${self}/home/profiles";
+  #
+  #   mod = "${self}/system";
+  #
+  #   desktop = [
+  #     ./core
+  #     ./core/boot.nix
+  #
+  #     ./hardware/fwupd.nix
+  #     ./hardware/graphics.nix
+  #
+  #     ./network
+  #
+  #     ./programs
+  #
+  #     ./services/greetd.nix
+  #     ./services/pipewire.nix
+  #   ];
+  #
+  #   laptop =
+  #     desktop
+  #     ++ [
+  #       ./hardware/bluetooth.nix
+  #     ];
+  #
+  #   # get the basic config to build on top of
+  #   inherit (import mod) laptop;
+  #
+  # in {
+  #   asura = nixosSystem {
+  #     inherit specialArgs;
+  #     modules =
+  #       laptop
+  #       ++ [
+  #         "${mod}/core/lanzaboote.nix"
+  #
+  #         "${mod}/hardware/nvidia-laptop.nix"
+  #
+  #         "${mod}/network/localsend.nix"
+  #         "${mod}/network/packettracer.nix"
+  #
+  #         "${mod}/programs/gamemode.nix"
+  #         "${mod}/programs/gaming.nix"
+  #         "${mod}/programs/hyprland"
+  #         "${mod}/programs/adb.nix"
+  #         "${mod}/programs/diagnostics.nix"
+  #         "${mod}/programs/arion.nix"
+  #
+  #         "${mod}/services/kanata"
+  #         "${mod}/services/syncthing.nix"
+  #         "${mod}/services/postgresql.nix"
+  #         {
+  #           home-manager = {
+  #             users.demi.imports = homeImports."demi@asura";
+  #             extraSpecialArgs = specialArgs;
+  #           };
+  #         }
 }

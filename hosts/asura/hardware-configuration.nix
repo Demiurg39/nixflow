1641410934b64e7c523cd683d5a366183b3a81bc
demiurg39 <chiryagov2014@gmail.com>
2025-10-13 22:32:14 +0600

    enhance btrfs filesystems conf

--- hosts/asura/hardware-configuration.nix
+++ hosts/asura/hardware-configuration.nix
@@ -10,23 +10,34 @@
   boot.initrd.kernelModules = [];
   boot.kernelParams = ["acpi_backlight=native"];
   boot.kernelModules = ["kvm-amd" "nvidia_uvm"];
+  boot.supportedFilesystems = ["btrfs"];
   boot.extraModulePackages = [];
 
-  # TODO: make subvolumes for /nix cause' most root files are symlinks from /nix/store
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "monthly";
+    fileSystems = ["/"];
+  };
+
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/b9eae7f6-9aab-428e-88dc-a9df3dbd72fb";
     fsType = "btrfs";
-    options = ["subvol=@" "compress=zstd"];
+    options = ["subvol=@" "compress=zstd" "noatime"];
   };
 
   fileSystems."/home" = {
     device = "/dev/disk/by-uuid/b9eae7f6-9aab-428e-88dc-a9df3dbd72fb";
     fsType = "btrfs";
-    options = ["subvol=@home"];
+    options = ["subvol=@home" "compress=zstd" "noatime"];
+  };
+
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/b9eae7f6-9aab-428e-88dc-a9df3dbd72fb";
+    fsType = "btrfs";
+    options = ["subvol=@nix" "compress=zstd" "noatime"];
   };
 
   fileSystems."/boot" = {
-    # device = "/dev/disk/by-uuid/BAB0-8E75";
     device = "/dev/disk/by-label/ESP";
     fsType = "vfat";
     options = ["fmask=0022" "dmask=0022"];

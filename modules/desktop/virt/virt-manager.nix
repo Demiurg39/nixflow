71c7f676ef2116dc491bf3c938487bc50680c277
demiurg39 <chiryagov2014@gmail.com>
2025-10-31 15:11:47 +0600

    remove ovmf since its available by default with qemu

--- modules/desktop/virt/virt-manager.nix
+++ modules/desktop/virt/virt-manager.nix
@@ -9,16 +9,6 @@
         runAsRoot = true;
         swtpm.enable = true;
         vhostUserPackages = [pkgs.virtiofsd];
-        ovmf = {
-          enable = true;
-          packages = [
-            (pkgs.OVMFFull.override {
-              secureBoot = true;
-              tpmSupport = true;
-            })
-            .fd
-          ];
-        };
       };
     };
     spiceUSBRedirection.enable = true;

bf4559ba75cf99cf02c80a675e4ebf4c07ff4af9
demiurg39 <chiryagov2014@gmail.com>
2026-01-07 16:03:23 +0600

    add tlp tweaks for laptop

--- modules/profiles/role/workstation.nix
+++ modules/profiles/role/workstation.nix
@@ -5,7 +5,25 @@
   ...
 }:
 with lib; let
+  nvidia = config.modules.nvidia;
   role = config.modules.profiles.role;
+  hardware = config.modules.profiles.hardware;
+
+  # nvidiaBusId = "PCI:01:0:0";
+  nvidiaPci = let
+    busId = nvidia.prime.nvidiaBusId;
+    cleanId = last (splitString "@" (removePrefix "PCI:" busId));
+    parts = splitString ":" cleanId;
+
+    pad = s:
+      if (stringLength s) < 2
+      then "0${s}"
+      else s;
+
+    bus = builtins.elemAt parts 0;
+    slot = pad (builtins.elemAt parts 1);
+    func = builtins.elemAt parts 2;
+  in "${bus}:${slot}.${func}";
 in
   mkMerge [
     (mkIf (hasPrefix "workstation" role)
@@ -65,8 +83,42 @@ in
     (mkIf (hasPrefix "workstation/laptop" role) {
       services.power-profiles-daemon.enable = mkForce false;
       services.tlp.enable = true;
+      services.tlp.settings = mkMerge [
+        {
+          # Change platform profile (Silent on battery)
+          PLATFORM_PROFILE_ON_AC = "performance";
+          PLATFORM_PROFILE_ON_BAT = "low-power";
 
-      # For deep-suspend mod
-      boot.kernelParams = ["mem_sleep_default=deep"];
+          # Disable cpu boos on battery
+          CPU_BOOST_ON_AC = 1;
+          CPU_BOOST_ON_BAT = 0;
+
+          # --- PCIe & NVMe ---
+          # Powersave for SSD and WiFi
+          PCIE_ASPM_ON_AC = "default";
+          PCIE_ASPM_ON_BAT = "powersupersave";
+
+          # --- Watchdog ---
+          # Disable Watchdog
+          NMI_WATCHDOG = 0;
+        }
+
+        (mkIf (any (mod: hasPrefix "gpu/nvidia" mod) hardware)
+          {
+            # Set nvidia power management to finegrained
+            RUNTIME_PM_ON_AC = "auto";
+            RUNTIME_PM_ON_BAT = "auto";
+            # Not managing nvidia device
+            RUNTIME_PM_DENYLIST = nvidiaPci;
+          })
+
+        (mkIf (any (p: hasPrefix "amd_pstate=active" p) config.boot.kernelParams) {
+          # amd_pstate tweaks
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        })
+      ];
     })
   ]

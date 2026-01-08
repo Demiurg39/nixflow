1637b4ffb29dc4e0c28549da27282a664c3b5daf
demiurg39 <chiryagov2014@gmail.com>
2026-01-08 16:47:15 +0600

    add gnome extensios and minos refactor

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -9,9 +9,12 @@ with lib; let
   hasPrime = config.modules.nvidia.prime.enable;
 
   extensions = [
-    # pkgs.gnomeExtensions.adaptive-brightness
-    # pkgs.gnomeExtensions.blur-my-shell
-    # pkgs.gnomeExtensions.appindicator
+    pkgs.gnomeExtensions.blur-my-shell
+    pkgs.gnomeExtensions.appindicator
+    pkgs.gnomeExtensions.clipboard-indicator
+    pkgs.gnomeExtensions.caffeine
+    pkgs.gnomeExtensions.just-perfection
+    pkgs.gnomeExtensions.vitals
   ];
 in {
   options.modules.desktop.gnome = with types; {
@@ -23,7 +26,7 @@ in {
       [
         papirus-icon-theme
         bibata-cursors
-        gnome-tweaks
+        refine # instead of gnome tweaks
       ]
       ++ extensions;
 
@@ -52,50 +55,30 @@ in {
       style = "adwaita-dark";
     };
 
-
     # Dconf settings
     programs.dconf.enable = true;
     home.extraConfig.dconf = {
       enable = true;
-      settings = mkMerge [
-        {
-          # set here colemak-dh as default layout
-          # set rus layout
-          # and set win_space_toggle
-          "org/gnome/desktop/interface" = {
-            accent-color = "blue";
-            color-scheme = "prefer-dark";
-          };
-          "org/gnome/desktop/input-sources" = {
-            xkb-options = ["ctrl:nocaps"];
-          };
+      settings = {
+        # set here colemak-dh as default layout
+        # set rus layout
+        # and set win_space_toggle
+        "org/gnome/desktop/interface" = {
+          accent-color = "blue";
+          color-scheme = "prefer-dark";
+        };
 
-          # Gnome extensions
-          "org/gnome/shell" = {
-            disable-user-extensions = false; # Optionally disable user extensions entirely
-            enabled-extensions = let
-              getUuid = ext:
-                if hasAttr "extensionUuid" ext
-                then ext.extensionUuid
-                # NOTE: maybe do something with ext.
-                # as i don't know how dconf will react
-                # As doc says: Alternatively, you can manually pass UUID as a string.
-                # "blur-my-shell@aunetx"
-                # ...
-                # Maybe tweak with metadata
-                else ext;
-            in (map getUuid extensions);
-          };
-        }
-
-        # Configure individual extensions
-        (mkIf (any (e: hasAttr "blur-my-shell" e) extensions) {
-          "org/gnome/shell/extensions/blur-my-shell" = {
-            brightness = 0.75;
-            noise-amount = 0;
-          };
-        })
-      ];
+        # Gnome extensions
+        "org/gnome/shell" = {
+          disable-user-extensions = false; # Optionally disable user extensions entirely
+          enabled-extensions = let
+            getUuid = ext:
+              if hasAttr "extensionUuid" ext
+              then ext.extensionUuid
+              else gvariant.mkNothing;
+          in (map getUuid extensions);
+        };
+      };
     };
 
     services.flatpak.enable = true;

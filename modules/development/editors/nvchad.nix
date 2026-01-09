556f873d0eaa591764f395d38fcb392284e3791f
demiurg39 <chiryagov2014@gmail.com>
2026-01-09 16:15:16 +0600

    add asm-lsp and asmfmt also refactor

--- modules/development/editors/nvchad.nix
+++ modules/development/editors/nvchad.nix
@@ -7,6 +7,26 @@
 }:
 with lib; let
   cfg = config.modules.development.editors.nvchad;
+  qmlls-wrapped = pkgs.writeShellScriptBin "qmlls" ''
+    QML_LIBS="${lib.makeSearchPath "lib/qt-6/qml" [
+      pkgs.kdePackages.qtdeclarative
+      pkgs.quickshell
+      pkgs.kdePackages.qt5compat
+    ]}"
+
+    PLUGIN_LIBS="${lib.makeSearchPath "lib/qt-6/plugins" [
+      pkgs.kdePackages.qtbase
+      pkgs.kdePackages.qtdeclarative
+      pkgs.quickshell
+    ]}"
+
+    QUICKSHELL="${config.flake.configDir}/quickshell"
+
+    export QML_IMPORT_PATH="$QML_LIBS:$QUICKSHELL:$PWD"
+    export QT_PLUGIN_PATH="$PLUGIN_LIBS"
+
+    exec ${pkgs.kdePackages.qtdeclarative}/bin/qmlls "$@"
+  '';
 in {
   options.modules.development.editors.nvchad = with types; {
     enable = mkEnableOption "Enable nushell";
@@ -15,38 +35,12 @@ in {
   config = mkIf (cfg.enable) {
     home-manager.users.${config.user.name}.imports = [inputs.nix4nvchad.homeManagerModule];
 
-    home.packages = let
-      qmlls-wrapped = pkgs.writeShellScriptBin "qmlls" ''
-        # 1. Формируем пути к библиотекам Nix
-        QML_LIBS="${lib.makeSearchPath "lib/qt-6/qml" [
-          pkgs.kdePackages.qtdeclarative
-          pkgs.quickshell
-          pkgs.kdePackages.qt5compat
-        ]}"
-
-        PLUGIN_LIBS="${lib.makeSearchPath "lib/qt-6/plugins" [
-          pkgs.kdePackages.qtbase
-          pkgs.kdePackages.qtdeclarative
-          pkgs.quickshell
-        ]}"
-
-        QUICKSHELL="${config.flake.configDir}/quickshell"
-
-        # 2. Экспортируем.
-        # Добавляем :$PWD в конец, чтобы видеть локальные модули в папке проекта
-        export QML_IMPORT_PATH="$QML_LIBS:$QUICKSHELL:$PWD"
-        export QT_PLUGIN_PATH="$PLUGIN_LIBS"
-
-        # 3. Запускаем оригинал
-        exec ${pkgs.kdePackages.qtdeclarative}/bin/qmlls "$@"
-      '';
-    in [
+    home.packages = [
       pkgs.cargo
       pkgs.rustc
       pkgs.gdb
       pkgs.mermaid-cli
       pkgs.python3
-      qmlls-wrapped
       pkgs.openjdk21
       pkgs.neovide
     ];
@@ -55,12 +49,15 @@ in {
       enable = true;
       extraPackages = [
         pkgs.alejandra
+        pkgs.asm-lsp
+        pkgs.asmfmt
         pkgs.nixd
         pkgs.nodejs
         pkgs.clang-tools
         pkgs.ghostscript
         pkgs.fd
         pkgs.lazygit
+        qmlls-wrapped
         pkgs.ripgrep
         pkgs.rustc
         pkgs.tectonic

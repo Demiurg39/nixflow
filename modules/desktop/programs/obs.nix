29e7714b5d1eb138cf702e480f9feaca9a874a63
demiurg39 <chiryagov2014@gmail.com>
2026-01-10 19:22:06 +0600

    add nvidia support for obs

--- modules/desktop/programs/obs.nix
+++ modules/desktop/programs/obs.nix
@@ -5,7 +5,22 @@
   ...
 }:
 with lib; let
+  hardware = config.modules.profiles.hardware;
   cfg = config.modules.desktop.programs.obs-studio;
+  nvidiaEnabled = any (mod: hasPrefix "gpu/nvidia" mod) hardware;
+  obs-with-nvidia = pkgs.symlinkJoin {
+    name = "obs-studio-nvidia";
+    paths = [pkgs.obs-studio];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/obs \
+        --set __NV_PRIME_RENDER_OFFLOAD 1 \
+        --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0 \
+        --set __GLX_VENDOR_LIBRARY_NAME nvidia \
+        --set __VK_LAYER_NV_optimus NVIDIA_only \
+        --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib:/run/opengl-driver-32/lib
+    '';
+  };
 in {
   options.modules.desktop.programs.obs-studio = {
     enable = mkEnableOption "TODO";
@@ -14,13 +29,14 @@ in {
   config = mkIf (cfg.enable) {
     programs.obs-studio = {
       enable = true;
+      package =
+        if nvidiaEnabled
+        then obs-with-nvidia
+        else pkgs.obs-studio;
 
-      plugins =
-        with pkgs.obs-studio-plugins; [
-          obs-pipewire-audio-capture
-        ]
-        # ++ (optionals (config.modules.desktop.wayland) ["wlrobs"])
-        ;
+      plugins = with pkgs.obs-studio-plugins; [
+        obs-pipewire-audio-capture
+      ];
     };
   };
 }

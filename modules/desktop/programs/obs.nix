a1c32de920360f8402cef9eef763e99c9ad3da95
demiurg39 <chiryagov2014@gmail.com>
2026-01-10 22:37:49 +0600

    refactor obs module

--- modules/desktop/programs/obs.nix
+++ modules/desktop/programs/obs.nix
@@ -8,19 +8,23 @@ with lib; let
   hardware = config.modules.profiles.hardware;
   cfg = config.modules.desktop.programs.obs-studio;
   nvidiaEnabled = any (mod: hasPrefix "gpu/nvidia" mod) hardware;
-  obs-with-nvidia = pkgs.symlinkJoin {
-    name = "obs-studio-nvidia";
-    paths = [pkgs.obs-studio];
-    buildInputs = [pkgs.makeWrapper];
-    postBuild = ''
-      wrapProgram $out/bin/obs \
-        --set __NV_PRIME_RENDER_OFFLOAD 1 \
-        --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0 \
-        --set __GLX_VENDOR_LIBRARY_NAME nvidia \
-        --set __VK_LAYER_NV_optimus NVIDIA_only \
-        --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib:/run/opengl-driver-32/lib
-    '';
-  };
+  obsPackage =
+    if nvidiaEnabled
+    then
+      pkgs.symlinkJoin {
+        name = "obs-studio-nvidia";
+        paths = [pkgs.obs-studio];
+        buildInputs = [pkgs.makeWrapper];
+        postBuild = ''
+          wrapProgram $out/bin/obs \
+            --set __NV_PRIME_RENDER_OFFLOAD 1 \
+            --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0 \
+            --set __GLX_VENDOR_LIBRARY_NAME nvidia \
+            --set __VK_LAYER_NV_optimus NVIDIA_only \
+            --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib:/run/opengl-driver-32/lib
+        '';
+      }
+    else pkgs.obs-studio;
 in {
   options.modules.desktop.programs.obs-studio = {
     enable = mkEnableOption "TODO";
@@ -29,11 +33,7 @@ in {
   config = mkIf (cfg.enable) {
     programs.obs-studio = {
       enable = true;
-      package =
-        if nvidiaEnabled
-        then obs-with-nvidia
-        else pkgs.obs-studio;
-
+      package = obsPackage;
       plugins = with pkgs.obs-studio-plugins; [
         obs-pipewire-audio-capture
       ];

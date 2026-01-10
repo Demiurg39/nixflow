902117716abe78631b62899671627788b49870d8
demiurg39 <chiryagov2014@gmail.com>
2026-01-10 22:10:57 +0600

    rewrite lutris module

--- modules/desktop/games/lutris.nix
+++ modules/desktop/games/lutris.nix
@@ -6,12 +6,28 @@
 }:
 with lib; let
   cfg = config.modules.desktop.games.lutris;
+  hardware = config.modules.profiles.hardware;
   steamCfg = config.modules.desktop.games.steam;
+  protonupPackage = config.modules.desktop.games.protonup.package;
+  nvidiaEnabled = any (mod: hasPrefix "gpu/nvidia" mod) hardware;
+  lutrisPackage =
+    if nvidiaEnabled
+    then
+      pkgs.lutris.override {
+        extraEnv = lib.mkIf nvidiaEnabled {
+          __NV_PRIME_RENDER_OFFLOAD = "1";
+          __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
+          __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+          __VK_LAYER_NV_optimus = "NVIDIA_only";
+        };
+      }
+    else pkgs.lutris;
 in {
   options.modules.desktop.games.lutris = {
     enable = mkEnableOption "TODO";
-    protonPackages = mkOpt' (types.listOf packages) [] "TODO";
-    winePackages = mkOpt' (types.listOf packages) [] "TODO";
+    package = mkOpt' (types.package) lutrisPackage "TODO";
+    protonPackages = mkOpt' (types.listOf types.package) [pkgs.proton-ge-bin] "TODO";
+    winePackages = mkOpt' (types.listOf types.package) [pkgs.wineWow64Packages.staging] "TODO";
   };
 
   config = mkIf (cfg.enable) {
@@ -20,19 +36,12 @@ in {
       extraPackages = [pkgs.mangohud];
       steamPackage =
         if (steamCfg.enable)
-        then osConfig.programs.steam.package
+        then steamCfg.package
         else pkgs.steam;
-      protonPackages = [(cfg.protonPackages ? pkgs.proton-ge-bin)];
-      winePackages = [(winePackages ? pkgs.wineWow64Packages.full)];
+      protonPackages = cfg.protonPackages;
+      winePackages = cfg.winePackages;
     };
 
-    home.packages = [pkgs.protonup-qt];
-    #   (lutris.override {
-    #     extraLibraries = pkgs: [
-    #       protobuf
-    #       libvdpau
-    #       glxinfo
-    #     ];
-    #   })
+    environment.systemPackages = [protonupPackage];
   };
 }

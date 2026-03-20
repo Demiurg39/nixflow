af0dc8d7291c524307425e39e602e35778ee4c6a
demiurg39 <chiryagov2014@gmail.com>
2026-03-20 20:12:56 +0600

    remove qmlls support for nvchad

--- modules/development/editors/nvchad.nix
+++ modules/development/editors/nvchad.nix
@@ -7,26 +7,6 @@
 }:
 with lib; let
   cfg = config.modules.development.editors.nvchad;
-  qmlls-wrapped = pkgs.writeShellScriptBin "qmlls" ''
-    QML_LIBS="${lib.makeSearchPath "lib/qt-6/qml" [
-      pkgs.kdePackages.qtdeclarative
-      pkgs.quickshell
-      pkgs.kdePackages.qt5compat
-    ]}"
-
-    PLUGIN_LIBS="${lib.makeSearchPath "lib/qt-6/plugins" [
-      pkgs.kdePackages.qtbase
-      pkgs.kdePackages.qtdeclarative
-      pkgs.quickshell
-    ]}"
-
-    QUICKSHELL="${config.flake.configDir}/quickshell"
-
-    export QML_IMPORT_PATH="$QML_LIBS:$QUICKSHELL:$PWD"
-    export QT_PLUGIN_PATH="$PLUGIN_LIBS"
-
-    exec ${pkgs.kdePackages.qtdeclarative}/bin/qmlls "$@"
-  '';
 in {
   options.modules.development.editors.nvchad = with types; {
     enable = mkEnableOption "Enable nushell";
@@ -57,7 +37,6 @@ in {
         pkgs.ghostscript
         pkgs.fd
         pkgs.lazygit
-        qmlls-wrapped
         pkgs.ripgrep
         pkgs.rustc
         pkgs.tectonic

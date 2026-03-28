2b8583493a0b0fd9c51cb157248904e7cd2bb9b3
demiurg39 <chiryagov2014@gmail.com>
2026-03-28 18:12:08 +0600

    add kitty config

--- modules/desktop/terminal/default.nix
+++ modules/desktop/terminal/default.nix
@@ -1,5 +1,15 @@
-{
+{lib, ...}:
+with lib; {
   imports = with builtins;
     map (fn: ./${fn})
     (filter (fn: fn != "default.nix") (attrNames (readDir ./.)));
+
+  options.modules.desktop.terminal = {
+    default = mkOpt (types.nullOr types.str) null;
+    spawnCmd = mkOpt' (types.nullOr types.str) null ''
+      Command to spawn new instance of terminal
+      If omitted will use just name of terminal
+      from config.modules.desktop.terminal.default
+    '';
+  };
 }
--- modules/desktop/terminal/kitty.nix
+++ modules/desktop/terminal/kitty.nix
@@ -1,22 +1,47 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }:
 with lib; let
-  cfg = config.modules.desktop.terminal.kitty;
+  cfg = termCfg.kitty;
+  termCfg = config.modules.desktop.terminal;
+  name = "kitty";
 in {
   options.modules.desktop.terminal.kitty = with types; {
     enable = mkEnableOption "Enable kitty terminal";
+    setDefault = mkOpt types.bool false;
   };
 
   config = mkIf (cfg.enable) {
+    modules.desktop.terminal.default =
+      if cfg.setDefault
+      then name
+      else "";
+    modules.desktop.terminal.spawnCmd =
+      if termCfg.default == name
+      then "kitty"
+      else "";
+    fonts.packages = [pkgs.nerd-fonts.jetbrains-mono];
+
     home.programs.kitty = {
       enable = true;
       keybindings = {};
       settings = {
-        font_family = "FiraCode Nerd Font";
+        font_family = "JetBrainsMono Nerd Font";
         font_size = 14;
+        window_padding_width = 5;
+        confirm_os_window_close = 0;
+        hide_window_decorations = true;
+        repaint_delay = 10;
+        input_delay = 3;
+        sync_to_monitor = true;
+        wayland_titlebar_color = "system";
+        cursor_shape = "block";
+        shell_integration = true;
+        background_opacity = 0.98;
+        background_blur = 64;
       };
     };
   };

e2710814bcf880c264ad0ec09060bb5d2824f661
demiurg39 <chiryagov2014@gmail.com>
2026-03-28 18:12:16 +0600

    add ghostty config

--- modules/desktop/terminal/ghostty.nix
+++ modules/desktop/terminal/ghostty.nix
@@ -6,6 +6,8 @@
 }:
 with lib; let
   cfg = config.modules.desktop.terminal.ghostty;
+  termCfg = config.modules.desktop.terminal;
+  name = "ghostty";
 in {
   options.modules.desktop.terminal.ghostty = {
     enable = mkEnableOption "Whether to enable ghostty";
@@ -15,29 +17,37 @@ in {
   config = mkIf (cfg.enable) {
     modules.desktop.terminal.default =
       if cfg.setDefault
+      then name
+      else "";
+    modules.desktop.terminal.spawnCmd =
+      if termCfg.default == name
       then "ghostty"
       else "";
-    modules.desktop.terminal.spawnCmd = "ghostty +new-window";
-    fonts.packages = [pkgs.nerd-fonts.iosevka-term];
+    fonts.packages = [pkgs.nerd-fonts.jetbrains-mono];
 
     home.programs.ghostty = {
       enable = true;
-      systemd.enable = true;
+      systemd.enable = false;
       settings = {
+        font-family = "JetBrainsMono Nerd Font";
+        adjust-font-baseline = 1;
+        adjust-cell-height = "15%";
+        minimum-contrast = 1.3;
+        font-size = 14;
         async-backend = "auto";
-        background-blur = 40;
-        background-blur-radius = 25;
-        background-opacity = 0.95;
+        background-blur = true;
+        background-opacity = 0.98;
         clipboard-paste-protection = true;
         copy-on-select = false;
         cursor-style = "block";
-        cursor-style-blink = false;
-        font-family = "Iosevka";
+        cursor-style-blink = true;
+        theme = "Black Metal";
         quit-after-last-window-closed = true;
         quit-after-last-window-closed-delay = "5m";
-        window-decoration = "none";
-        window-padding-x = 5;
-        window-padding-y = 3;
+        window-decoration = false;
+        window-padding-x = 12;
+        window-padding-y = 10;
+        window-padding-balance = true;
         window-vsync = true;
       };
     };

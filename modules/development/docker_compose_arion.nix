0a45784cab6d7cea6cc21f11cdcab6a439975ecb
demiurg39 <chiryagov2014@gmail.com>
2026-05-05 16:44:22 +0600

    add nvidia support to docker containers

--- modules/development/docker_compose_arion.nix
+++ modules/development/docker_compose_arion.nix
@@ -6,12 +6,17 @@
   ...
 }:
 with lib; let
+  # TODO: rename it to just docker, or podman smth, and
+  # fix any possible issues that might pop out
   cfg = config.modules.development.docker-compose;
+  hasNvidia = any (mod: hasPrefix "gpu/nvidia" mod) config.modules.profiles.hardware;
 in {
   options.modules.development.docker-compose = with types; {
     enable = mkEnableOption "Whether to enable docker compose wia arion";
   };
   config = mkIf (cfg.enable) {
+    hardware.nvidia-container-toolkit.enable = hasNvidia;
+
     environment.systemPackages = [
       pkgs.arion
       pkgs.docker-client
@@ -22,6 +27,7 @@ in {
     virtualisation.docker.enable = false;
     virtualisation.podman = {
       enable = true;
+      dockerCompat = true;
       dockerSocket.enable = true;
       defaultNetwork.settings.dns_enabled = true;
     };
